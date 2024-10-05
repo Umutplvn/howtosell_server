c9f7c33cd64a6332cbfa4a5029d7c338acbcda09
umutplvn <umutpehlivan2078@gmail.com>
2024-10-05 07:32:55 +0100

    up

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -10,10 +10,10 @@ const User = require("../models/user");
 module.exports = {
   create: async (req, res) => {
   
-    const {email, name, age, lastname, phone, instagram, occupationm, descOfJob, income, goal, obstacles, directInvest} = req.body.user;
+    const {email, name, age, lastname, phone, instagram, occupation, descOfJob, income, goal, obstacles, directInvest} = req.body.user;
 
-    const data = await User.create({email, name, age, lastname, phone, instagram, occupationm, descOfJob, income, goal, obstacles, directInvest});
-    sendEmail(email, name, age, lastname, phone, instagram, occupationm, descOfJob, income, goal, obstacles, directInvest)
+    const data = await User.create({email, name, age, lastname, phone, instagram, occupation, descOfJob, income, goal, obstacles, directInvest});
+    sendEmail(email, name, age, lastname, phone, instagram, occupation, descOfJob, income, goal, obstacles, directInvest)
     res.send({
       error: false,
       result: data,
