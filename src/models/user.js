dfb2ff5ea499d38b644b7020826ffc841d1d2a03
umutplvn <umutpehlivan2078@gmail.com>
2024-10-05 06:36:56 +0100

    up

--- src/models/user.js
+++ src/models/user.js
@@ -7,11 +7,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    age: { type: Number, required: true },
-    name: { type: String, required: true },
-    lastname: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true },
+    age: { type: Number },
+    name: { type: String },
+    lastname: { type: String },
+    email: { type: String, unique: true },
+    phone: { type: String },
     instagram: { type: String },
     occupation: { type: String },
     descOfJob: { type: String },
