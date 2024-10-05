7d880732e52cb1ea82ed77fec6a77f25fb5da426
umutplvn <umutpehlivan2078@gmail.com>
2024-10-05 21:50:45 +0100

    up

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -10,10 +10,11 @@ const User = require("../models/user");
 module.exports = {
   create: async (req, res) => {
   
-    const {email, name, age, lastname, phone, instagram, occupation, descOfJob, income, goal, obstacles, directInvest} = req.body.user;
+    const {email, name, age, lastname, phone, instagram, occupation, descOfJob, income, goal, obstacles, directInvest} = req.body;
 
     const data = await User.create({email, name, age, lastname, phone, instagram, occupation, descOfJob, income, goal, obstacles, directInvest});
-    sendEmail(email, name, age, lastname, phone, instagram, occupation, descOfJob, income, goal, obstacles, directInvest)
+    // sendEmail(email, name, age, lastname, phone, instagram, occupation, descOfJob, income, goal, obstacles, directInvest)
+
     res.send({
       error: false,
       result: data,
--- src/models/user.js
+++ src/models/user.js
@@ -7,18 +7,18 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    age: { type: String },
-    name: { type: String },
-    lastname: { type: String },
-    email: { type: String },
-    phone: { type: String },
-    instagram: { type: String },
-    occupation: { type: String },
-    descOfJob: { type: String },
-    income: { type: String },
-    goal: { type: String },
-    obstacles: { type: String },
-    directInvest: { type: String },
+    age: { type: String, trim:true },
+    name: { type: String, trim:true },
+    lastname: { type: String, trim:true },
+    email: { type: String, trim:true },
+    phone: { type: String, trim:true },
+    instagram: { type: String, trim:true },
+    occupation: { type: String, trim:true },
+    descOfJob: { type: String, trim:true },
+    income: { type: String, trim:true },
+    goal: { type: String, trim:true },
+    obstacles: { type: String, trim:true },
+    directInvest: { type: String, trim:true },
 });
 
 const User = mongoose.model('User', userSchema);
